bf8563f7426a0c9bfe953e1cc4fb9b036ccb4620
Anthony Dugarte <34623660+prfcto2@users.noreply.github.com>
2018-04-13 15:23:23 -0400

    Update 3091.cpp

--- COJ/3091.cpp
+++ COJ/3091.cpp
@@ -2,65 +2,38 @@
 #include <vector>
 #include <algorithm>
 
-/*
-#define TESTING_TIME
-#include <ctime>
-#include <iomanip>//*/
-
-
 using namespace std;
 
 void seek(size_t& deadline, int64_t warlocks_p, vector<size_t>& power, size_t& harry_p, int64_t& track)
 {
-  size_t begining{1};
-  size_t l{0};
+  size_t it{0};
 
-  for(l; l < deadline; ++l)
+  if((warlocks_p  += power[it++]) > harry_p)
+    return;
+
+  while((warlocks_p  += power[it++]) <= harry_p and it <= deadline);
+
+  if(--it > 15)
+    --track += static_cast<int64_t>(1 << (it - 15)) *  1 << 15;
+  else --track += 1 << it;
+
+  for(it; it < deadline; warlocks_p  += power[++it])
   {
-    warlocks_p += power[l];
-
-    if(warlocks_p <= harry_p)
-      continue;
-
-    if(not l)
-      break;
-
-    if(begining)
-      track += ((1 << l) - (1 << begining--) + 1);
-
     size_t it_aux {0};
     int64_t warlocks_p_aux{warlocks_p};
 
-    for(it_aux; it_aux < l; ++it_aux)
-    {
-      warlocks_p_aux -= power[it_aux];
-      if(warlocks_p_aux > harry_p)
-        continue;
+    if((warlocks_p_aux -= power[it_aux]) <= harry_p)
+    	++track;
 
-      ++track;
-      if(it_aux)
-        seek(it_aux, warlocks_p_aux, power, harry_p, track);
-    }
+    while(++it_aux < it)
+      if((warlocks_p_aux -= power[it_aux]) <= harry_p)
+        seek(it_aux, warlocks_p_aux, power, harry_p, ++track);
   }
-
-  if(begining)
-    if(l > 15)
-    {
-      size_t aux {15};
-      l -= aux;
-
-      int64_t aux_aux{1 << l};
-      aux_aux *= (1 << aux);
-
-      track += --aux_aux;
-    }
-    else track += ((1 << l) - (1 << begining) + 1);
 }
 
 int main()
 {
-  size_t warlocks_n;
-  size_t harry_power;
+  size_t warlocks_n, harry_power;
   cin >> warlocks_n >> harry_power;
 
   vector<size_t> warlocks(warlocks_n);
@@ -71,21 +44,10 @@ int main()
   while(warlocks.back() > harry_power)
     warlocks.pop_back();
 
-  #ifdef TESTING_TIME
-  clock_t prove = clock();
-  #endif
-
   int64_t track{0};
   seek(warlocks_n, 0, warlocks, harry_power, track);
   cout << track << endl;
 
-
-  #ifdef TESTING_TIME
-  prove = clock() - prove;
-  cout << setprecision(10);
-  cout << fixed;
-  cout << static_cast<double>(prove)/CLOCKS_PER_SEC << endl;
-  #endif
-
   return 0;
 }
+
