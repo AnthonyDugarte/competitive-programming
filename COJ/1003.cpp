88e4c834dcf121fd151a203c212e128967b6a33e
Anthony Dugarte <34623660+prfcto2@users.noreply.github.com>
2018-04-18 06:31:55 -0400

    Update 1003.cpp

--- COJ/1003.cpp
+++ COJ/1003.cpp
@@ -11,30 +11,30 @@ int main()
   cin >> cases;
   vector<size_t> winners;
 
-  while (--cases >= 0)
+  do
   {
     size_t candidates;
     size_t regions;
     cin >> candidates >> regions;
     vector<size_t> votes(candidates);
 
-    while(--regions >= 0)
+    do
     {
-      for(size_t candidate = 0; candidate < candidates; ++candidate)
+      for(size_t candidate {0}; candidate < candidates; ++candidate)
       {
         size_t votes_achieved;
         cin >> votes_achieved;
-        votes[candidate]+=votes_achieved;
+        votes[candidate] += votes_achieved;
       }
-    }
-    size_t winner(0);
+    }while(--regions);
 
-    for(size_t it = 0; it < candidates; ++it)
+    size_t winner{0};
+    for(size_t it {0}; it < candidates; ++it)
       if(votes[it] > votes[winner])
         winner = it;
 
     winners.push_back(move(++winner));
-  }
+  }while(--cases);
 
   for(auto& it : winners)
     cout << it << "\n";
