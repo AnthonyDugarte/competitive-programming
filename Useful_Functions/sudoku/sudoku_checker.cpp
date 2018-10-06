79105628b18bbad20a3f4a1443fcdeb0f57bd2d8
Anthony Dugarte <toonny1998@gmail.com>
2018-10-05 20:45:36 -0400

    updated checker method for a test case that could make the checker fail

--- Useful_Functions/sudoku/sudoku_checker.cpp
+++ Useful_Functions/sudoku/sudoku_checker.cpp
@@ -10,9 +10,16 @@ using vi_t = std::vector<type_t>;
 template<typename T> // 1 + 2 + 3 + 4 + ... + n
 T naturalSummatory(T n) { return n * (n + 1) / 2; }
 
-// O(n * n) + O(2n) = O(n * n)
+type_t correct_sum;
+bool check_eq(type_t val)
+{
+  return val == correct_sum;
+}
+
+// O(n * n) + O(2n) + O(n * n) = O(n * n)
 bool is_sudoku_ok(const std::vector<vi_t> &table)
 {
+  // checking rows and cols
   vi_t row_sums(table.size(), 0);
   vi_t col_sums(table.size(), 0);
 
@@ -25,11 +32,39 @@ bool is_sudoku_ok(const std::vector<vi_t> &table)
     }
   }
 
-  auto check_eq = [correct_sum = naturalSummatory(table.size())](auto &&val)
-  { return val == correct_sum; };
+  correct_sum = naturalSummatory(table.size());
 
-  return (std::all_of(row_sums.begin(), row_sums.end(), check_eq) &&
-              std::all_of(col_sums.begin(), col_sums.end(), check_eq));
+  if (not (std::all_of(row_sums.begin(), row_sums.end(), &check_eq) &&
+              std::all_of(col_sums.begin(), col_sums.end(), &check_eq)))
+    return false;
+
+    // validating subgrids
+    size_t grid_size { static_cast<size_t>(std::sqrt(table.size())) };
+
+    for(size_t x_grid{ 0 }; x_grid != table.size(); x_grid += grid_size)
+    {
+      for(size_t y_grid{ 0 }; y_grid != table.size(); y_grid += grid_size)
+      {
+        size_t row_start{ static_cast<size_t>(x_grid / grid_size) };
+        size_t col_start{ static_cast<size_t>(y_grid / grid_size) };
+
+        std::vector<bool> reps(grid_size, false);
+
+        for(size_t x{ row_start * grid_size }; x < (row_start + 1) * grid_size; ++x)
+        {
+          for(size_t y{ col_start * grid_size }; y < (col_start + 1) * grid_size; ++y)
+          {
+            if(reps[table[x][y]] == true)
+              return false;
+
+            reps[table[x][y]] = true;
+          }
+        }
+      }
+    }
+
+    // grids all are well placed
+    return true;
 }
 
 std::vector<vi_t> getTable(size_t n = 9)
