d89dc878b48f3b191c5b01c8a602cad4c57ba7d7
anthony <anthony@ignisgravitas.com>
2019-01-04 14:56:48 -0400

    Educational Codeforces Round 57

---
+++ CodeForces/FindDivisible.cpp
@@ -0,0 +1,18 @@
+// Educational Codeforces Round 57
+#include <bits/stdc++.h>
+
+int main()
+{
+    size_t t;
+    std::cin >> t;
+
+    while(--t != -1)
+    {
+        size_t l, r;
+        std::cin >> l >> r;
+
+        std::cout << l << " " << (l << 1) << "\n";
+    }
+
+    return 0;
+}
