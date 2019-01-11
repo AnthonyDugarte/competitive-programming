5833de45a0591bbe09f8108db61bf22d53019913
anthony <anthony@ignisgravitas.com>
2019-01-11 12:42:48 -0400

    little styled script for cp problems

---
+++ CodeForces/__script.cpp
@@ -0,0 +1,41 @@
+/*
+ *
+ * Copyright: Anthony Dugarte
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+using std::cin;
+using std::cout;
+
+template<typename T, typename U>
+using p_t = std::pair<T, U>;
+
+template<typename T>
+using s_t = std::set<T>;
+template<typename T>
+using v_t = std::vector<T>;
+
+template<typename T, typename U>
+using m_t = std::map<T, U>;
+template<typename T, typename U>
+using um_t = std::unordered_map<T, U>;
+
+using type_t = int64_t;
+using utype_t = uint64_t;
+using str_t = std::string;
+
+
+void fastIO() {
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {
+    // fastIO();
+
+
+    return 0;
+}
