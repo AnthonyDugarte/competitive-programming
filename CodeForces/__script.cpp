882294754ae327b03fd7c52f4721955bca510343
anthony <anthony@ignisgravitas.com>
2019-01-20 11:53:26 -0400

    reduce script size

--- CodeForces/__script.cpp
+++ CodeForces/__script.cpp
@@ -1,9 +1,3 @@
-/*
- *
- * 
- *
- */
-
 #include <bits/stdc++.h>
 
 using std::cin;
@@ -16,18 +10,13 @@ void fastIO() {
     cout.tie(nullptr);
 }
 
-template<typename T, typename U>
-using p_t = std::pair<T, U>;
+template<typename T, typename U> using p_t = std::pair<T, U>;
 
-template<typename T>
-using s_t = std::set<T>;
-template<typename T>
-using v_t = std::vector<T>;
+template<typename T> using s_t = std::set<T>;
+template<typename T> using v_t = std::vector<T>;
 
-template<typename T, typename U>
-using m_t = std::map<T, U>;
-template<typename T, typename U>
-using um_t = std::unordered_map<T, U>;
+template<typename T, typename U> using m_t = std::map<T, U>;
+template<typename T, typename U> using um_t = std::unordered_map<T, U>;
 
 using type_t = int64_t;
 using utype_t = uint64_t;
