aec62d4739371e0872a66919a06d0437651eee91
Anthony Dugarte <toonny1998@gmail.com>
2019-03-03 08:58:35 -0400

    upate script

--- CodeForces/__script.cpp
+++ CodeForces/__script.cpp
@@ -1,10 +1,5 @@
 #include <bits/stdc++.h>
 
-using std::cin;
-using std::cout;
-
-void fastIO(bool = false);
-
 template <typename T, typename U>
 using p_t = std::pair<T, U>;
 
@@ -14,6 +9,8 @@ template <typename T>
 using us_t = std::unordered_set<T>;
 template <typename T>
 using v_t = std::vector<T>;
+template <typename T>
+using vv_t = std::vector<std::vector<T>>;
 
 template <typename T, typename U>
 using m_t = std::map<T, U>;
@@ -24,26 +21,58 @@ using type_t = int64_t;
 using utype_t = uint64_t;
 using str_t = std::string;
 
-template <typename T>
-T min_val() { return std::numeric_limits<T>::max(); }
-template <typename T>
-T max_val() { return std::numeric_limits<T>::max(); }
-type_t gcd(type_t a, type_t b) { return a == 0 ? b : gcd(b % a, a); }
+using vt = v_t<type_t>;
+using vut = v_t<utype_t>;
+using vstr = v_t<str_t>;
+
+using vvt = vv_t<type_t>;
+using vvut = vv_t<utype_t>;
+using vvstr = vv_t<str_t>;
+
+using std::cin;
+using std::cout;
+
+constexpr bool MULT_TESTS = false,
+               UNTIE_IO = false;
 
 constexpr char endl = '\n';
-constexpr type_t module{static_cast<type_t>(1e9 + 7)};
+constexpr type_t M{static_cast<type_t>(1e9 + 7)};
+
+void fastIO(bool = false);
+template <typename T>
+void debug(T val) { cout << val << endl; }
+template <typename T, typename... TT>
+void debug(T val, TT... tail) { cout << val << " : ", debug(tail...); }
+
+template <typename T>
+T min_val() { return std::numeric_limits<T>::min(); }
+template <typename T>
+T max_val() { return std::numeric_limits<T>::max(); }
+
+type_t gcd(type_t a, type_t b) { return a == 0 ? b : gcd(b % a, a); }
+
+void code(size_t t)
+{
+    // code here :3
+}
+
+void use_tests(size_t t)
+{
+    cin >> t;
+    for (size_t i{0}; i < t; ++i)
+        code(i);
+}
 
 int main()
 {
-    fastIO(/* true */);
-
+    fastIO(UNTIE_IO);
+    (MULT_TESTS ? use_tests : code)(0);
     return 0;
 }
 
 void fastIO(bool untie)
 {
     std::ios_base::sync_with_stdio(false);
-
     if (untie)
     {
         cin.tie(nullptr);
