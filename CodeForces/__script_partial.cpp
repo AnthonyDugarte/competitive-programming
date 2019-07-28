6148ebbe607fe803e4bcaebd82f12114e71bac05
Anthony Dugarte <toonny1998@gmail.com>
2019-07-28 11:48:47 -0400

    Add partial CP script

---
+++ CodeForces/__script_partial.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+template <typename T>
+using v_t = std::vector<T>;
+
+using type_t = int64_t;
+using utype_t = uint64_t;
+using str_t = std::string;
+
+using std::cin;
+using std::cout;
+
+constexpr char endl = '\n';
+
+template <typename T>
+void debug(T val) { cout << val << endl; }
+template <typename T, typename... TT>
+void debug(T val, TT... tail) { cout << val << " : ", debug(tail...); }
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+
+    return 0;
+}
